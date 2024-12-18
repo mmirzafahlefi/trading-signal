652fa6756c3c2e2ccaffef192816dd886e563dfb
mirzafahlefi0910 <mirzafahlefi0910@gmail.com>
2024-12-18 19:11:49 +0000

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,29 +71,9 @@ export default function Home() {
     <div className="relative bg-yellow-50 overflow-hidden max-h-screen text-black">
       <header className="fixed right-0 top-0 left-60 bg-yellow-50 py-3 px-4 h-16">
         <div className="max-w-4xl mx-auto">
-          <div className="flex items-center justify-between">
-            <div>
-              <button type="button" className="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-yellow-300 focus:border-yellow-300 transition">
-                <span className="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-white transition mr-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
-                  </svg>
-                </span>
-                <span className="text-sm">Archive</span>
-              </button>
-            </div>
+          <div className="flex items-center justify-center">
             <div className="text-lg font-bold">
               {socketConnected ? "WebSocket Connected ✅" : "WebSocket Disconnected ❌"}
-            </div>
-            <div>
-              <button type="button" className="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-yellow-300 focus:border-yellow-300 transition">
-                <span className="text-sm">This week</span>
-                <span className="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-white transition ml-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-                  </svg>
-                </span>
-              </button>
             </div>
           </div>
         </div>
@@ -108,7 +88,7 @@ export default function Home() {
             <div className="p-4">
               <ul className="space-y-1">
                 <li>
-                  <a href="javascript:void(0)" className="flex items-center bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4">
+                  <a href="/" className="flex items-center bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
                       <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
                     </svg>Bot
@@ -172,14 +152,6 @@ export default function Home() {
                             </select>
                         </div>
                     </div>
-
-                    {/* Tombol Submit */}
-                    {/* <button
-                      type="submit"
-                      className="inline-flex items-center justify-center py-2 px-3 rounded-xl bg-white text-gray-800 hover:text-green-500 text-sm font-semibold transition"
-                    >
-                      Get Signal
-                    </button> */}
                   </div>
                   <div className="flex items-center gap-x-2">
                     <button type="submit" className="inline-flex items-center justify-center h-9 px-5 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
@@ -210,14 +182,6 @@ export default function Home() {
                         </div>
                       )}
                     </div>
-                    {/* <div className="p-4 bg-yellow-100 rounded-xl text-gray-800">
-                      <div className="font-bold text-2xl leading-none">20</div>
-                      <div className="mt-2">Tasks finished</div>
-                    </div>
-                    <div className="p-4 bg-yellow-100 rounded-xl text-gray-800">
-                      <div className="font-bold text-2xl leading-none">5,5</div>
-                      <div className="mt-2">Tracked hours</div>
-                    </div> */}
                     <div className="col-span-2">
                       <div className="p-4 bg-purple-100 rounded-xl text-gray-800">
                         <div className="font-bold text-xl leading-none">History</div>
